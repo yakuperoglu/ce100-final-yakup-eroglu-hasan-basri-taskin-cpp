4749ddc903b64b08af70b3bb2c7c50e3385e1d3e
yakup <yakup_eroglu22@erdogan.edu.tr>
2024-05-26 12:04:04 +0300

    app.cpp reorganized

--- src/Taskschedulerapp/src/Taskschedulerapp.cpp
+++ src/Taskschedulerapp/src/Taskschedulerapp.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file main.cpp
+ * @brief Main file for the Task Scheduler application.
+ *
+ * This file contains the main function which initiates the Task Scheduler application by calling the main menu.
+ * The application is designed to manage and schedule tasks efficiently using various algorithms.
+ */
+
 #include <iostream>
 #include <stack>
 #include <string>
@@ -7,9 +15,23 @@
 
 using namespace std;
 
+/**
+ * @brief The entry point of the Task Scheduler application.
+ *
+ * This function serves as the entry point for the Task Scheduler application.
+ * It calls the mainMenu function to display the main menu of the application.
+ *
+ * @param in Input stream for the application, typically std::cin.
+ * @param out Output stream for the application, typically std::cout.
+ * @return int Returns 0 upon successful execution.
+ *
+ * @exception std::runtime_error Throws a runtime error if the mainMenu function encounters an issue.
+ *
+ * @note The mainMenu function is responsible for managing user interactions and executing the appropriate task scheduling algorithms.
+ */
 int main() {
 
-    mainMenu(cin, cout);
-
-    return 0;
+	mainMenu(cin, cout);
+	return 0;
 }
+
